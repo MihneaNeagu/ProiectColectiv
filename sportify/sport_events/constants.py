4e43f4c31b2b3aadea967da6f5ebd1c3ace5d657
MoghiroiuBogdan <91489628+MoghiroiuBogdan@users.noreply.github.com>
2023-11-04 17:45:50 +0200

    added sport events

---
+++ sportify/sport_events/constants.py
@@ -0,0 +1,9 @@
+TIP_SPORT = [
+    ("", "Select a SPORT:"),
+    ("FOTBAL"),
+    ("HANDBALL"),
+    ("VOLLEY"),
+    ("GYM"),
+    ("TENIS"),
+    ("PING-PONG"),
+    ("BILIARD")]
---
+++ sportify/sport_events/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.db import models
+
+sport_event = get_user_model()
+
+class Sport_Event(models.Model):
+    sport_type =
+# Create your views here.

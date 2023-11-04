8899411508627916d2db7941602b4c54cf60d8fd
Sergiu Neag <sergiu.neag@stud.ubbcluj.ro>
2023-11-04 18:14:58 +0200

    Added models for user app

--- sportify/users/models.py
+++ sportify/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+
+class User(models.Model):
+    username = models.CharField(max_length=255, null=True)
+    password = models.CharField(max_length=255, null=True)
--- sportify/users/views.py
+++ sportify/users/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
-from django.contrib.auth import get_user_model
-from django.db import models
-
-
-User = get_user_model()
-
-#Base model
-
-class User(models.Model):
-    username = models.CharField(max_length=255)
 
 # Create your views here.

bf274274a524cdf854726d1a3cfda358e40121d2
Rubben22 <pasca.ruben120@gmail.com>
2023-12-07 00:20:40 +0200

    sport

---
+++ sportify/sports/constants.py
@@ -0,0 +1,9 @@
+TIP_SPORT = [
+    ("", "Select a SPORT:"),
+    ("FOTBAL", "Football"),
+    ("BRAZILIAN JIU JITSU", "Bjj"),
+    ("MIXED MARTIAL ARTS", "MMA"),
+    ("KICKBOX", "Kickbox"),
+    ("TENIS", "Tennis"),
+    ("BOX", "Box"),
+]

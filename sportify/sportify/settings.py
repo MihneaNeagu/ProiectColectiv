51728e756614c595e4c5f456eac85ce86ec08654
MoraruVladGabriel <moraruvlad51@yahoo.com>
2023-12-10 17:40:46 +0200

    user complete

--- sportify/sportify/settings.py
+++ sportify/sportify/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'sportify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR /  "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- sportify/sportify/urls.py
+++ sportify/sportify/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user/', include('users.urls'))
+    path('admin/', admin.site.urls)
 ]

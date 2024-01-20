fadbc5e31f966e7bc41f53fecebb4ee598d64779
darsius <p_darius211@yahoo.com>
2024-01-20 13:28:53 +0200

    added tests for users model

--- sportify/users/tests.py
+++ sportify/users/tests.py
@@ -5,28 +5,23 @@ User = get_user_model()
 
 class UserModelTest(TestCase):
     def setUp(self):
-        # Create a user for testing
         self.user = User.objects.create(username='testuser', password='testpassword')
 
     def test_user_creation(self):
-        # Test that the user is created successfully
         self.assertEqual(self.user.username, 'testuser')
         self.assertEqual(self.user.password, 'testpassword')
 
     def test_unique_username_constraint(self):
-        # Test that the username field has a unique constraint
         duplicate_user = User(username='testuser', password='anotherpassword')
         with self.assertRaises(Exception):
             duplicate_user.save()
 
     def test_null_username_constraint(self):
-        # Test that the username field does not allow null values
         user_with_null_username = User(username=None, password='testpassword')
         with self.assertRaises(Exception):
             user_with_null_username.save()
 
     def test_null_password_constraint(self):
-        # Test that the password field does not allow null values
         user_with_null_password = User(username='testuser', password=None)
         with self.assertRaises(Exception):
             user_with_null_password.save()
